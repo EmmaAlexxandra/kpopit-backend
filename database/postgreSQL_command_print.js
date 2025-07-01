65b4e018c945ce5dd36f4cdef771d92c6e0260dc
Emma Secrest <esecrest908@gmail.com>
2025-07-01 16:12:52 -0400

    script to generate insert commands

--- database/postgreSQL_command_print.js
+++ database/postgreSQL_command_print.js
@@ -19,5 +19,24 @@ async function loadFile(filePath) {
 const groups = await loadFile("./kpopit_groups.csv");
 const idols = await loadFile("./kpopit_idols.csv");
 
-console.log("Groups:", groups.slice(0, 10));
-console.log("Idols:", idols.slice(0, 10));
+
+function generateGroupInserts(groups) {
+    return groups.map(group => {
+      return `INSERT INTO groups (debut_date, group_name, hex_code) VALUES ('${group.debut_date}', '${group.group_name}', '${group.hex_code}');`;
+    }).join('\n');
+  }
+  
+  function generateIdolInserts(idols) {
+    return idols.map(idol => {
+      return `INSERT INTO idols (DOB, group, stage_name, legal_name) VALUES ('${idol.DOB}', '${idol.group}', '${idol.stage_name}', '${idol.legal_name}');`;
+    }).join('\n');
+  }
+
+  const groupSQL = generateGroupInserts(groups);
+const idolSQL = generateIdolInserts(idols);
+
+const fullSQL = `${groupSQL}\n\n${idolSQL}`;
+
+
+await fs.writeFile("kpopit_inserts.txt", fullSQL, "utf-8");
+console.log("SQL insert commands written to kpopit_inserts.txt");
