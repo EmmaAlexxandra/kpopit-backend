40e753e0a0df03580550889cbdaf6f2b789ecc96
EmmaAlexxandra <esecrest908@gmail.com>
2025-07-22 16:52:59 -0400

    edit: moving the create post after all the get functions

--- src/routes/posts/postController.ts
+++ src/routes/posts/postController.ts
@@ -1,44 +1,5 @@
 import { Request, Response } from 'express';
 import pool from '../pool'
-
-export async function createPost(req: Request, res: Response) {
-    var {userID,content,groupID,idolBirthday} = req.body;
-    console.log(req.body)
-
-    if (!userID || !content ) {
-        res.status(400).json({ error: 'Missing required fields' });
-        return;
-
-    } else if (!groupID){
-        groupID = null
-    } else if (!idolBirthday) {
-        idolBirthday = '[]';
-    }
-
-    const parsedContext = typeof content === 'string' ? JSON.parse(content) : content;
-    const parsedIdolBirthday = typeof idolBirthday === 'string' ? JSON.parse(idolBirthday) : idolBirthday;
-    
-    try {
-        const userCheck = await pool.query('SELECT * FROM users WHERE id = $1', [userID]);
-        if (userCheck.rows.length === 0) {
-            res.status(404).json({ error: 'User not found' });
-            return;
-        }
-
-        const result = await pool.query(
-            `INSERT INTO posts (user_id, content, group_id, idol_birthday) 
-             VALUES ($1, $2, $3, $4) RETURNING *;`,
-            [userID, JSON.stringify(parsedContext), groupID, JSON.stringify(parsedIdolBirthday)]
-        );
-
-        res.status(201).json(result.rows[0]);
-
-    } catch (error: any) {
-        console.error('❌ Error creating post:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-}
-
 export async function getAllPosts(req: Request, res: Response) {
     try {
         const result = await pool.query('SELECT * FROM posts');
@@ -150,6 +111,45 @@ export async function getPostsByGroupNameOrIdolName(req:Request, res:Response){
         res.status(500).json({ error: 'Internal server error' });
     }
 }
+
+export async function createPost(req: Request, res: Response) {
+    var {userID,content,groupID,idolBirthday} = req.body;
+    console.log(req.body)
+
+    if (!userID || !content ) {
+        res.status(400).json({ error: 'Missing required fields' });
+        return;
+
+    } else if (!groupID){
+        groupID = null
+    } else if (!idolBirthday) {
+        idolBirthday = '[]';
+    }
+
+    const parsedContext = typeof content === 'string' ? JSON.parse(content) : content;
+    const parsedIdolBirthday = typeof idolBirthday === 'string' ? JSON.parse(idolBirthday) : idolBirthday;
+    
+    try {
+        const userCheck = await pool.query('SELECT * FROM users WHERE id = $1', [userID]);
+        if (userCheck.rows.length === 0) {
+            res.status(404).json({ error: 'User not found' });
+            return;
+        }
+
+        const result = await pool.query(
+            `INSERT INTO posts (user_id, content, group_id, idol_birthday) 
+             VALUES ($1, $2, $3, $4) RETURNING *;`,
+            [userID, JSON.stringify(parsedContext), groupID, JSON.stringify(parsedIdolBirthday)]
+        );
+
+        res.status(201).json(result.rows[0]);
+
+    } catch (error: any) {
+        console.error('❌ Error creating post:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
 
 // TODO: these two need to be changed to track if they have like the post previously
 export async function putLikePost(req: Request, res: Response) {
@@ -289,3 +289,4 @@ export async function putAddCommentToPost(req:Request, res:Response){
         res.status(500).json({ error: 'Internal server error' });
     }
 }
+
